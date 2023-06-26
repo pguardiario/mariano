788f424d4700218c1b9f3d3b3eaac5908e437150
P Guardiario <p.guardiario@gmail.com>
2023-06-26 14:24:05 +0800

    init

---
+++ img.py
@@ -0,0 +1,39 @@
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.models import Model
+import numpy as np
+from PIL import Image
+
+base_model = VGG16(weights='imagenet')
+model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
+
+def extract(img):
+    img = img.resize((224, 224)) # Resize the image
+    img = img.convert('RGB') # Convert the image color space
+    x = image.img_to_array(img) # Reformat the image
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    feature = model.predict(x)[0] # Extract Features
+    return feature / np.linalg.norm(feature)
+
+# Iterate through images and extract Features
+images = ["130.png", "250.png", "280.png", "340.png", "95.png", ]
+
+
+
+
+
+
+
+all_features = np.zeros(shape=(len(images),4096))
+
+for i in range(len(images)):
+    feature = extract(img=Image.open(images[i]))
+    all_features[i] = np.array(feature)
+
+# Match image
+query = extract(img=Image.open("275.png")) # Extract its features
+dists = np.linalg.norm(all_features - query, axis=1) # Calculate the similarity (distance) between images
+ids = np.argsort(dists)[:5] # Extract 5 images that have lowest distance
+
+print(dists, ids)
